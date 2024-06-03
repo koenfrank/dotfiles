d02017011a72e1c3ccf1bd51425db36e305d3010
Koen Franklin <151389664+koenfrank@users.noreply.github.com>
2024-06-03 12:43:59 +0200

    revert codelens

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -10,10 +10,10 @@ return {
       },
       -- Enable this to enable the builtin LSP code lenses on Neovim >= 0.10.0
       -- Be aware that you also will need to properly configure your LSP server to
-      -- -- provide the code lenses.
-      -- codelens = {
-      --   enabled = true,
-      -- },
+      -- provide the code lenses.
+      codelens = {
+        enabled = true,
+      },
       servers = {
         omnisharp = {
           handlers = {
