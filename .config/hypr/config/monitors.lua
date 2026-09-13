dcab38f247fe57aefbbeb968978aa8895c187a6d
Koen Frank <151389664+koenfrank@users.noreply.github.com>
2026-09-13 11:21:03 +0200

    new laptop, cachyos

---
+++ .config/hypr/config/monitors.lua
@@ -0,0 +1,23 @@
+hl.monitor({
+	output = "DP-7",
+	mode = "1920x1080@120",
+	position = "0x0",
+	scale = 1,
+	transform = 0,
+})
+
+hl.monitor({
+	output = "DP-1",
+	mode = "5120x1440@120",
+	position = "1920x0",
+	scale = 1,
+	transform = 0,
+})
+
+hl.monitor({
+	output = "DP-5",
+	mode = "2560x1080",
+	position = "7040x0",
+	scale = 1,
+	transform = 3,
+})
